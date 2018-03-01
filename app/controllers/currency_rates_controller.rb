016c3bd07ef462f1c97fabb146432effa0a689a4
Lena Smolnikova <smolnik1404@gmail.com>
2018-03-01 22:38:48 +0300

    Fix whitespaces

--- app/controllers/currency_rates_controller.rb
+++ app/controllers/currency_rates_controller.rb
@@ -1,6 +1,6 @@
 class CurrencyRatesController < ApplicationController
-	def index
-		@currencies = CurrencyRate.order(:updated_at)
-		@average_values = CurrencyRate.average_values
-	end
+  def index
+    @currencies = CurrencyRate.order(:updated_at)
+    @average_values = CurrencyRate.average_values
+  end
 end
--- app/parsers/currency_parser.rb
+++ app/parsers/currency_parser.rb
@@ -1,27 +1,27 @@
 class CurrencyParser
-	attr_accessor :rates
-	def initialize(response)
-		@rates = response.fetch('payload', {}).fetch('rates', {}) 
-	end
+  attr_accessor :rates
+  def initialize(response)
+    @rates = response.fetch('payload', {}).fetch('rates', {}) 
+  end
 
-	def rub_deposit_payments
-		rub_deposit_payments = deposit_payments.select { |dp| dp.fetch('toCurrency', {}).fetch('name') == 'RUB' }
-		{
-			usd_sell: get_value('USD', 'sell', rub_deposit_payments),
-  		usd_buy: get_value('USD', 'buy', rub_deposit_payments),
-			eur_sell: get_value('EUR', 'sell', rub_deposit_payments),
-			eur_buy: get_value('EUR', 'buy', rub_deposit_payments)			
-		}
-	end
+  def rub_deposit_payments
+    rub_deposit_payments = deposit_payments.select { |dp| dp.fetch('toCurrency', {}).fetch('name') == 'RUB' }
+    {
+      usd_sell: get_value('USD', 'sell', rub_deposit_payments),
+      usd_buy: get_value('USD', 'buy', rub_deposit_payments),
+      eur_sell: get_value('EUR', 'sell', rub_deposit_payments),
+      eur_buy: get_value('EUR', 'buy', rub_deposit_payments)
+    }
+  end
 
-	def deposit_payments
-		rates.select {|rate| rate['category'] == 'DepositPayments'}
-	end
+  def deposit_payments
+    rates.select {|rate| rate['category'] == 'DepositPayments'}
+  end
 
-	def get_value(currency, op, rates)
-		rates
-		  .select {|rate| rate.fetch('fromCurrency', {})['name'] == currency}
-		  .first
-		  .try(:[], op)
-	end
+  def get_value(currency, op, rates)
+    rates
+      .select {|rate| rate.fetch('fromCurrency', {})['name'] == currency}
+      .first
+      .try(:[], op)
+  end
 end
