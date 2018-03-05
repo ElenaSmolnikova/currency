cbc35c22394162b946f6460c5723aabbc2392838
Lena Smolnikova <smolnik1404@gmail.com>
2018-03-05 12:35:46 +0300

    Fix division by zero

--- app/models/currency_rate.rb
+++ app/models/currency_rate.rb
@@ -2,6 +2,7 @@ class CurrencyRate < ApplicationRecord
   class << self
     [:usd_sell, :usd_buy, :eur_sell, :eur_buy].each do |operation|
       define_method :"#{operation}_average" do
+        return if count.zero?
         (all.sum(operation)/count).round(2)
       end
     end
